c559c8b1f4e7b8afb388156bb57d91647e123139
Jonas. L <33558637+Eksno@users.noreply.github.com>
2021-11-24 13:00:19 +0100

    Add some echos

--- run_docker.bat
+++ run_docker.bat
@@ -6,11 +6,20 @@ set MYDIR1=%MYDIR:~0,-1%
 
 for %%f in (%MYDIR1%) do set myfolder=%%~nxf
 
-echo Removing old container.
+
+echo.
+echo Removing old container:
+
+echo - Stopping old container...
 docker stop %myfolder%
+echo - Removing old container...
 docker rm %myfolder%
 
-echo Running %myfolder% in new container.
+
+echo.
+echo Running %myfolder% in new container:
+
+echo - Running container...
 docker run -p 8080:8080 --name=%myfolder% %myfolder%
 
 PAUSE
